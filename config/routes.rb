d80cb6cdd5a14ef273448c947fca1b71988d2024
Achal Channarasappa <achannar@andrew.cmu.edu>
2014-05-01 13:14:50 -0400

    started work on families

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ ChessCamp::Application.routes.draw do
   resources :users
   resources :sessions
   resources :locations
+  resources :families
 
   # semi-static routes
   get 'home', to: 'home#index', as: :home
