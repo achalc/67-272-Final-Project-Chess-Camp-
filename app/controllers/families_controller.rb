478e8c1c3ecd4fdbf97646d58257edb8a3a15623
Achal Channarasappa <achannar@andrew.cmu.edu>
2014-05-01 19:50:44 -0400

    family tests all pass

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -2,8 +2,8 @@ class FamiliesController < ApplicationController
   before_action :set_family, only: [:show, :edit, :update, :destroy]
 
   def index
-    @active_families = Family.active.alphabetical.paginate(:page => params[:page].per_page(10))
-    @inactive_families = Family.inactive.alphabetical.paginate(:page => params[:page].per_page(10))
+    @active_families = Family.active.alphabetical.paginate(:page => params[:page]).per_page(10)
+    @inactive_families = Family.inactive.alphabetical.paginate(:page => params[:page]).per_page(10)
 
   end
 
@@ -28,7 +28,7 @@ class FamiliesController < ApplicationController
 
   def update
     if @family.update(family_params)
-      redirect_to @family, notice: "#{@family.family_name} was revised in the system."
+      redirect_to @family, notice: "#{@family.family_name} family was revised in the system."
     else
       render action: 'edit'
     end
@@ -36,7 +36,7 @@ class FamiliesController < ApplicationController
 
   def destroy
     @location.destroy
-    redirect_to families_path, notice: "#{@family.family_name} was removed from the system."
+    redirect_to families_path, notice: "#{@family.family_name} family was removed from the system."
   end
 
   private
