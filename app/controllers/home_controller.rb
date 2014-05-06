5f028f9d9eacb7432227fd7e9fe3c24a5198d8b9
Achal Channarasappa <achannar@andrew.cmu.edu>
2014-05-06 13:48:48 -0400

    chartkick on ins dash

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,11 @@ class HomeController < ApplicationController
   def index
   	if !current_user.nil?
   		@instructor = current_user.instructor
-  	end
+      @upcoming_camps = @instructor.camps.upcoming.chronological.map { |e| e.name }
+      @registration_count = @instructor.camps.upcoming.chronological.map { |camp| Registration.where("camp_id = ?", camp.id).count  }
+  	  @camp_to_registration = Hash[@upcoming_camps.zip @registration_count]
+      # redirect_to @instructor, notice: "#{@camp_to_registration}"
+    end
   end
 
   def about
