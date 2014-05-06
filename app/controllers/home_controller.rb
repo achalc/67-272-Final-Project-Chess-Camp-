c4cb156b5a6b250ca1531049a2d7abf5e59bce1c
Achal Channarasappa <achannar@andrew.cmu.edu>
2014-05-06 18:19:31 -0400

    3 graphs on admin dash

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,10 +7,16 @@ class HomeController < ApplicationController
       @registration_count = @instructor.camps.upcoming.chronological.map { |camp| Registration.where("camp_id = ?", camp.id).count  }
   	  @camp_to_registration_instructor = Hash[@upcoming_camps.zip @registration_count]
       # this provides data needed for the admin dashboard
+      # For Pie Graph
       @camps = Camp.all.alphabetical
+      # For Bar Graph
       @instructors = CampInstructor.all.map { |e| e.instructor.proper_name }
       @camps_taught = CampInstructor.all.map { |r| CampInstructor.where('instructor_id = ?', r.instructor_id).count }
       @camps_to_instructor = Hash[@instructors.zip @camps_taught]
+      # Registrations per camp
+      @upcoming_camps_admin = Camp.all.upcoming.chronological.map { |e| e.name.to_s + ' on ' + humanize_date(e.start_date)  }
+      @registration_count_admin = Camp.all.upcoming.chronological.map { |camp| Registration.where("camp_id = ?", camp.id).count  }
+      @camp_to_registration_admin = Hash[@upcoming_camps_admin.zip @registration_count_admin]
     end
   end
 
