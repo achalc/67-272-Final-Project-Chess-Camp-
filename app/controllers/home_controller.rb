73e6d763be1dedd178204176b3c16c5d19bbaf50
Achal Channarasappa <achannar@andrew.cmu.edu>
2014-05-06 14:04:39 -0400

    chart works

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
   	if !current_user.nil?
   		@instructor = current_user.instructor
-      @upcoming_camps = @instructor.camps.upcoming.chronological.map { |e| e.name }
+      @upcoming_camps = @instructor.camps.upcoming.chronological.map { |e| e.name.to_s + ' on ' + humanize_date(e.start_date) }
       @registration_count = @instructor.camps.upcoming.chronological.map { |camp| Registration.where("camp_id = ?", camp.id).count  }
   	  @camp_to_registration = Hash[@upcoming_camps.zip @registration_count]
       # redirect_to @instructor, notice: "#{@camp_to_registration}"
